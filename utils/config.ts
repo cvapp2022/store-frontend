ced0636be5c9902ed5629e6688fc8aed3581b8cf
cvapp2022 <cvapp2022@gmail.com>
2023-03-20 18:43:06 +0300

    update baseUrl

--- utils/config.ts
+++ utils/config.ts
@@ -1,4 +1,4 @@
-export const baseUrl='http://127.0.0.1:8000/api'
+// export const baseUrl='http://127.0.0.1:8000/api'
 
-// export const baseUrl='https://storeapp.cleverapps.io/api'
+export const baseUrl='https://storeapp.cleverapps.io/api'
 
